2d6932c0fd60b682d7ad8a7e78b3d579a5f24925
leonraz58 <leonraz58@gmail.com>
2025-04-21 17:17:51 +0300

    add TheTypography

---
+++ src/components/TheTypography.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+defineProps(
+  {
+    p3: Boolean,
+    p1: Boolean,
+    h3: Boolean,
+    h2: Boolean,
+    h1: Boolean,
+  }
+)
+</script>
+
+
+<template>
+  <p :class="{
+    'the-typography__p_p3': p3,
+    'the-typography__p_p1': p1,
+    'the-typography__p_h3': h3,
+    'the-typography__p_h2': h2,
+    'the-typography__p_h1': h1,
+  }"
+     class="the-typography the-typography__p"
+  >
+    <slot></slot>
+  </p>
+</template>
+
+<style lang="scss">
+.the-typography__p {
+  &_p3 {
+    font-size: var(--font-size-s);
+    line-height: var(--line-height-s);
+  }
+
+  &_p1 {
+    line-height: var(--line-height-l);
+  }
+
+  &_h3 {
+    line-height: var(--line-height-l);
+    font-weight: var(--font-weight-medium);
+  }
+
+  &_h2 {
+    font-size: var(--font-size-l);
+    line-height: var(--line-height-xl);
+    font-weight: var(--font-weight-medium);
+  }
+
+  &_h1 {
+    font-size: var(--font-size-xl);
+    line-height: var(--line-height-xxxl);
+    font-weight: var(--font-weight-medium);
+  }
+}
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './assets/index.scss'
 import '@fontsource/ubuntu/400.css';
 import '@fontsource/ubuntu/700.css';
+import '@fontsource/ubuntu/500.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
