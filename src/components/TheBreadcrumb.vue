5eb56e63a701b404cc8c47786feec45757ea355f
leonraz58 <leonraz58@gmail.com>
2025-04-23 03:28:30 +0300

    add TheBreadcrumb

---
+++ src/components/TheBreadcrumb.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TheTypography from '@/components/TheTypography.vue'
+</script>
+
+<template>
+  <div class="the-breadcrumb the-breadcrumb__container">
+    <TheTypography><a href="/">Главная</a> >
+      <slot></slot>
+    </TheTypography>
+  </div>
+</template>
+
+<style lang="scss">
+  .the-breadcrumb {
+    &__container {
+      margin-top: 32px;
+    }
+  }
+</style>
