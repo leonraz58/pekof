2470703d16b4de116c2cf3b0b783dae026879d0a
leonraz58 <leonraz58@gmail.com>
2025-04-27 20:07:29 +0300

    fix loader

--- src/components/TheLoader.vue
+++ src/components/TheLoader.vue
@@ -1,17 +1,27 @@
-<script setup lang="ts"></script>
+<template>
+  <div class="the-loader the-loader__container">
+    <span class="the-loader__elem"></span>
+  </div>
+</template>
 
-<template><span class="loader"></span></template>
-
-<style scoped lang="scss">
-.loader {
-  width: 48px;
-  height: 48px;
-  border: 5px solid var(--primary-500);
-  border-bottom-color: var(--primary-900);
-  border-radius: 50%;
-  display: inline-block;
-  box-sizing: border-box;
-  animation: rotation 1s linear infinite;
+<style lang="scss">
+.the-loader {
+  &__elem {
+    width: 48px;
+    height: 48px;
+    border: 5px solid var(--primary-500);
+    border-bottom-color: var(--primary-900);
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+  }
+  &__container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: var(--main-height);
+  }
 }
 
 @keyframes rotation {
