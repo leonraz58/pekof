5a94f0d1f09fe8799f66874bad2754a8774028a6
leonraz58 <leonraz58@gmail.com>
2025-04-28 12:55:52 +0300

    add NotFound page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import PageContainer from '@/components/PageContainer.vue'
+import TheTypography from '@/components/TheTypography.vue'
+</script>
+
+<template>
+  <PageContainer centered>
+    <TheTypography tag="h1" tag-variant="h1">Page not found</TheTypography>
+  </PageContainer>
+</template>
+
+<style scoped lang="scss"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/components/HomePage.vue'
 import FavouritesPage from '@/components/FavouritesPage.vue'
 import ProductPage from '@/components/ProductPage.vue'
+import NotFound from '@/components/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,8 +18,9 @@ const router = createRouter({
     },
     {
       path: '/product/:id',
-      component: ProductPage
+      component: ProductPage,
     },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
   ],
 })
 
